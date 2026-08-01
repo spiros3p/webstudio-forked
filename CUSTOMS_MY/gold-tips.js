d5d9c7f85232923ee1b830febad98b0ed5deb99d
spiros3p <spiros3p@gmail.com>
2026-08-01 12:30:58 +0300

    pnpm-lock updated + added custom files generic

---
+++ CUSTOMS_MY/gold-tips.js
@@ -0,0 +1,38 @@
+"mailto:numadriving@gmail.com?subject=Inquiry%20Form&body=" +
+  `
+Vehicle: ${system.search.ctrlRadioVehicle}%0A
+Package: ${system.search.ctrlRadioPackage}%0A
+Number of People: ${system.search.noOfPeople}%0A
+Date: ${system.search.date}%0A
+Pick up from: ${system.search.pickup}%0A
+Destination: ${system.search.destination}%0A
+Name: ${system.search.name}%0A
+Phone: ${system.search.tel}%0A
+Email: ${system.search.email}%0A
+Message: %0A${system.search.message}%0A
+`;
+"https://wa.me/32492510607?text=" +
+  `
+Vehicle: ${system.search.ctrlRadioVehicle}%0A
+Package: ${system.search.ctrlRadioPackage}%0A
+Number of People: ${system.search.noOfPeople}%0A
+Date: ${system.search.date}%0A
+Pick up from: ${system.search.pickup}%0A
+Destination: ${system.search.destination}%0A
+Name: ${system.search.name}%0A
+Phone: ${system.search.tel}%0A
+Email: ${system.search.email}%0A
+Message: %0A${system.search.message}%0A
+`;
+"https://t.me/numa_private_transfersbot?text=" +
+  `Vehicle: ${system.search.ctrlRadioVehicle}%0A
+Package: ${system.search.ctrlRadioPackage}%0A
+Number of People: ${system.search.noOfPeople}%0A
+Date: ${system.search.date}%0A
+Pick up from: ${system.search.pickup}%0A
+Destination: ${system.search.destination}%0A
+Name: ${system.search.name}%0A
+Phone: ${system.search.tel}%0A
+Email: ${system.search.email}%0A
+Message: %0A${system.search.message}%0A
+`;
